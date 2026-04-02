5f663b578c9a2393bf751e5cf285b72552eb87c4
Damian0563 <damian.piechocki05@gmail.com>
2026-04-02 18:21:32 +0200

    Setup for authorization using supabase

--- backend/database.go
+++ backend/database.go
@@ -1,11 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func createUser(mail string, secret string, appName string) {
-	fmt.Printf(mail, secret, appName)
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context.Background())
+	hashedSecret, err := bcrypt.GenerateFromPassword([]byte(secret), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	_, err = conn.Exec(context.Background(), "INSERT INTO users (mail, secret, app_name) VALUES ($1, $2, $3)", mail, string(hashedSecret), appName)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func signIn(mail string, secret string) {
-	fmt.Printf(mail, secret)
+func signIn(mail string, secret string) bool {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context.Background())
+
+	var hashedSecret string
+	err = conn.QueryRow(context.Background(), "SELECT secret FROM users WHERE mail = $1", mail).Scan(&hashedSecret)
+	if err != nil {
+		panic(err)
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(hashedSecret), []byte(secret))
+	return err == nil
 }
