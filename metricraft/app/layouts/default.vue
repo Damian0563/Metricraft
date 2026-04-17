13849497f35e231bf4a4ff782f817aaf37d6e610
Damian0563 <damian.piechocki05@gmail.com>
2026-04-17 17:40:46 +0200

    Simplyfying and fixing default layout for overflows

--- metricraft/app/layouts/default.vue
+++ metricraft/app/layouts/default.vue
@@ -1,24 +1,5 @@
 <template>
-	<div class="layout bg-black" :class="overflow ? 'h-auto' : 'h-screen'">
+	<div class="layout bg-black min-h-screen">
 		<slot />
 	</div>
 </template>
-
-
-<script setup lang="ts">
-
-const overflow = ref(false)
-const checkOverflow = () => {
-	overflow.value = document.documentElement.scrollHeight > window.innerHeight
-}
-watch(overflow, () => {
-	checkOverflow()
-})
-onMounted(() => {
-	checkOverflow()
-	window.addEventListener('resize', checkOverflow)
-})
-onUnmounted(() => {
-	window.removeEventListener('resize', checkOverflow)
-})
-</script>
