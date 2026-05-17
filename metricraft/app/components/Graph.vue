7952261904c2c5ce128d6aa992bf55ccadbaf021
Damian0563 <damian.piechocki05@gmail.com>
2026-05-17 19:14:29 +0200

    Start some work on the charts, this is fucked up

--- metricraft/app/components/Graph.vue
+++ metricraft/app/components/Graph.vue
@@ -1,15 +1,31 @@
 <template>
 	<div class="flex flex-col rounded shadow-lg bg-white w-full h-48 md:h-64 lg:h-96 text-black aspect-square">
-		<h1 class="text-2xl font-bold text-center">{{ props.name }}</h1>
+		<h1 class="text-2xl font-bold text-center mt-2">{{ props.name }}</h1>
+		<canvas ref="chartRef"></canvas>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { Chart } from "chart.js";
+import { onMounted } from "vue";
+import { createTrafficCongestionTrends } from "~/composables/charts";
 const props = defineProps<{
 	name: string;
 	data: any;
 }>();
+const chartRef = ref<HTMLCanvasElement | null>(null);
+let chartInstance: Chart | null = null;
+onMounted((): void => {
+	populateChart(props.data);
+});
+watch(() => props.data, (newData: any): void => {
+	if (chartInstance) chartInstance.destroy();
+	populateChart(newData);
+});
 
-console.log(props.data)
-
+const populateChart = (data: any): void => {
+	if (props.name === "Traffic congestion trends" && chartRef.value) {
+		chartInstance = createTrafficCongestionTrends(chartRef.value, data);
+	}
+}
 </script>
