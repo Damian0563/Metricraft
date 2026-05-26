27bb74991bb2be925177843ef3c0b8a92d92d57a
Damian Piechocki <damian.piechocki05@gmail.com>
2026-05-26 19:59:31 +0200

    Update backend/mailing.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- backend/mailing.go
+++ backend/mailing.go
@@ -99,6 +99,8 @@ const verificationEmailTemplate = `<!DOCTYPE html>
 </html>`
 
 func renderVerificationEmail(to, code string) []byte {
+	// Sanitize to prevent header injection
+	to = strings.ReplaceAll(strings.ReplaceAll(to, "\r", ""), "\n", "")
 	body := strings.ReplaceAll(verificationEmailTemplate, "{{CODE}}", html.EscapeString(code))
 	headers := "From: Metricraft <noreply.metricraft@gmail.com>\r\n" +
 		"To: " + to + "\r\n" +
